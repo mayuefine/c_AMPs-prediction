aba10e5cd62c81e5dbd03a1713933b300023cb29
mayuefine <mayue@im.ac.cn>
2019-01-13 16:10:14 +0800

    Update blast-v.sh

--- Script/blast-v.sh
+++ Script/blast-v.sh
@@ -1,1 +1,3 @@
-vsearch --usearch_global 1.fa -db q.fa --id 0.95 --threads 4 --strand both --maxaccepts 0 --maxrejects 0 --userout longer-test.txt --userfields query+target+id+alnlen+mism+opens+qcov+tcov
+vsearch --usearch_global query.fa -db db.fa --id 0.95 --threads 4 --strand both --maxaccepts 0 --maxrejects 0 --userout longer-test.txt --userfields query+target+id+alnlen+mism+opens+qcov+tcov
+# query.fa: prediction sequences
+# db.fa: those we colleted AMPs sequences
