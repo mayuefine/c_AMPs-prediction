2e38aa97542b6f8db97b060a51a24aa97cb5657c
mayuefine <mayue@im.ac.cn>
2019-01-17 13:23:58 +0800

    Update blast-v.sh

--- Script/predict_AMP/blast-v.sh
+++ Script/predict_AMP/blast-v.sh
@@ -1,3 +1,6 @@
-vsearch --usearch_global query.fa -db db.fa --id 0.95 --threads 4 --strand both --maxaccepts 0 --maxrejects 0 --userout longer-test.txt --userfields query+target+id+alnlen+mism+opens+qcov+tcov
+formatdb -i all_AMP.fa -p T
+#-p means protein
+blastp -query pred_amp_sequence_2.txt -db all_AMP.fa -out one_sample_test.txt -outfmt 6
 # query.fa: prediction sequences
 # db.fa: those we colleted AMPs sequences
+# output file title are: query id, subject id, % identity, alignment length, mismatches, gap opens, q. start, q. end, s. start, s. end, evalue, bit score
