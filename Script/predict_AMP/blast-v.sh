96e0c24d5eb8d5954c2a12413fc27b8904559ebb
mayuefine <mayue@im.ac.cn>
2019-02-15 11:39:31 +0800

    Update blast-v.sh

--- Script/predict_AMP/blast-v.sh
+++ Script/predict_AMP/blast-v.sh
@@ -1,5 +1,5 @@
 formatdb -i all_AMP.fa -p T
 #-p means protein
 blastp -query pred_amp_sequence_2.txt -db all_AMP.fa -out one_sample_test.txt -outfmt "6 qseqid sseqid pident length mismatch gaps qcovs" -max_target_seqs 1 -num_threads 8
-# query.fa: prediction sequences
+# query.fa: prediction sequences, this file is coming from after_prediction.pl script
 # db.fa: those we colleted AMPs sequences
