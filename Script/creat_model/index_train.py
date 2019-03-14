a8330c3341dd6052f27e40216ce44caf952af88d
mayuefine <mayue@im.ac.cn>
2019-03-14 14:34:31 +0800

    Update index_train.py

--- Script/creat_model/index_train.py
+++ Script/creat_model/index_train.py
@@ -6,9 +6,9 @@ from Attention import Attention_layer
 from keras import backend as K
 import ROC
 
-model = load_model('20f20b_10_0.9800_16.h5', custom_objects={'Attention_layer': Attention_layer})
-x = loadtxt("x_train_16.txt", delimiter=",")
-y = loadtxt("y_train_16.txt", delimiter=",")
+model = load_model('20f20b_09_0.9690_03.h5', custom_objects={'Attention_layer': Attention_layer})
+x = loadtxt("x_train_03.txt", delimiter=",")
+y = loadtxt("y_train_03.txt", delimiter=",")
 preds_x = model.predict(x).tolist()
 
 tp = 0
@@ -56,16 +56,18 @@ def ap_at_k(number, sorted_list_t):
             break
     return sum(pr)/number, j
 
+
+len_slt = list(s_l_t[x][0][1] == 2 for x in arange(0, len(y))).count(True)
 set_number = int(input("Please input the number K for AP@K: "))
-if set_number >= len(y):
-    print("The number you seted is bigger than %.0f, which is our total number of this test data, please reset it"%len(y))
-    set_number = len(y)
+if set_number >= len_slt:
+    print("The number you seted is bigger than %.0f, which is our total correct number of this test data, please reset it"%len_slt)
+    set_number = len_slt
 apk = ap_at_k(set_number, s_l_t)
 
 print("AP@K, K = %.0f: %.2f%%, and the last predition number is %.9f"%(set_number, apk[0]*100, s_l_t[apk[1]][0][0]))
 print("Accuracy: %.2f%%"%(acc*100))
 print("Sensitivity: %.2f%%"%(sens*100))
 print("Specificity: %.2f%%"%(spec*100))
-print("MCC: %.4f"%(mcc))
+print("MCC: %.4f"% mcc)
 print("auROC: %.2f%%"%(auc*100))
 print("Precision: %.2f%%"%(precision*100))
